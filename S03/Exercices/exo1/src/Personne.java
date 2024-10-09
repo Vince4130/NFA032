6977641b0c78db7eb9ba00839db9696ec5f3ad60
Vince4130 <vinceblanca@gmail.com>
2024-10-09 18:40:12 +0200

    exo s03

---
+++ S03/Exercices/exo1/src/Personne.java
@@ -0,0 +1,31 @@
+
+public class Personne { 
+
+    public String nom;
+    public String prenom;
+    public int age;
+    public String statutMatrimonial; 
+    
+    public String getNomComplet (String nom, String prenom) {       
+        
+        String nomComplet = prenom+nom; 
+        return nomComplet; 
+    } 
+    
+    public int getAge () {       
+        return age; 
+    } 
+
+    public void changeStatut (String newStatut) { 
+        statutMatrimonial = newStatut;     
+    }      
+        
+    public static void main(String[] args) { 
+        // System.out.println("It is " + (args != null && args.length >= 2));
+        Personne = new Personne();
+        System.out.println();
+        System.out.println('Bonjour ' + );
+
+    } 
+}
+
